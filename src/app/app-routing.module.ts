7087c48888349889395ae6fb9ded539357621f32
hemonth <hemonthmandava@gmail.com>
2019-02-03 13:47:06 -0600

    application without ngrx module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {ShellComponent} from './home/shell.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {PageNotFoundComponent} from './home/page-not-found.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {path: 'welcome', component: WelcomeComponent},
+            {
+                path: 'products',
+                // canActivate: [AuthGuard],
+                loadChildren: './products/product.module#ProductModule'
+            },
+            {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        ]
+    },
+    {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
