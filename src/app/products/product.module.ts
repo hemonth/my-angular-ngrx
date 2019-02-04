2361715cf91236ca9069efdccac1cbc385a0cc64
hemonth <hemonthmandava@gmail.com>
2019-02-03 22:32:39 -0600

    ngrx store with reducer passing previous state and the action requested returning an updated state.

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,19 +7,25 @@ import {ProductShellComponent} from './product-shell/product-shell.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductEditComponent} from './product-edit/product-edit.component';
 
+/*NgRx*/
+import {StoreModule} from '@ngrx/store';
+import {reducer} from './state/product.reducer';
+
 const productRoutes: Routes = [
-  { path: '', component: ProductShellComponent }
+    {path: '', component: ProductShellComponent}
 ];
 
 @NgModule({
-  imports: [
-    SharedModule,
-    RouterModule.forChild(productRoutes)
-  ],
-  declarations: [
-    ProductShellComponent,
-    ProductListComponent,
-    ProductEditComponent
-  ]
+    imports: [
+        SharedModule,
+        RouterModule.forChild(productRoutes),
+        StoreModule.forFeature('products', reducer)
+    ],
+    declarations: [
+        ProductShellComponent,
+        ProductListComponent,
+        ProductEditComponent
+    ]
 })
-export class ProductModule { }
+export class ProductModule {
+}
